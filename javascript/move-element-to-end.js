4481081279d5e7d6da608c9cd52a02ec64b03bc7
Kevin Bastoul <kaycbas@gmail.com>
2021-01-29 22:26:01 -0800

    improve solution

--- javascript/move-element-to-end.js
+++ javascript/move-element-to-end.js
@@ -1,4 +1,4 @@
-// O(n) Time | O(1) Space - where n is the length of the array
+// O(n) Time (if implemented with linked list) | O(1) Space - where n is the length of the array
 function moveElementToEnd(array, toMove) {
 	let count = 0;
 	for (let i = array.length - 1; i >= 0; i--) {
@@ -8,4 +8,21 @@ function moveElementToEnd(array, toMove) {
 		}
 	}
 	return array;
+}
+
+// Better
+// O(n) Time | O(1) Space - where n is the length of the array
+function moveElementToEnd(array, toMove) {
+  let i = 0;
+	let j = array.length - 1;
+	while (i < j) {
+		while (i < j && array[j] === toMove) j--;
+		if (array[i] === toMove) swap(array, i, j);
+		i++;
+	}
+	return array;
+}
+
+const swap = (array, i, j) => {
+	[array[i], array[j]] = [array[j], array[i]]
 }
