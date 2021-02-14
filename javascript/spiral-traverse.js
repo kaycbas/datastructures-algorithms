9cea31183d5f3a89a0272542ff374262efd914de
Kevin Bastoul <kaycbas@gmail.com>
2021-02-13 17:04:31 -0800

    add spiral traverse

---
+++ javascript/spiral-traverse.js
@@ -0,0 +1,30 @@
+// O(n) Time | O(n) Space - where n is the number of elements in the array
+function spiralTraverse(array) {
+  let startRow = 0;
+	let endRow = array.length - 1;
+	let startCol = 0;
+	let endCol = array[0].length - 1;
+	
+	const result = [];
+	while (startRow <= endRow && startCol <= endCol) {
+		for (let col = startCol; col <= endCol; col++) {
+			result.push(array[startRow][col]);
+		}
+		for (let row = startRow + 1; row <= endRow; row++) {
+			result.push(array[row][endCol]);
+		}
+		for (let col = endCol - 1; col >= startCol; col--) {
+			if (startRow === endRow) break;
+			result.push(array[endRow][col]);
+		}
+		for (let row = endRow - 1; row > startRow; row--) {
+			if (startCol === endCol) break;
+			result.push(array[row][startCol]);
+		}
+		startCol++;
+		endCol--;
+		startRow++;
+		endRow--;
+	}
+	return result;
+}
