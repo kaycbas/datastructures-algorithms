6c2e337599c728e9c9bb72411a80554d9d6755f3
Kevin Bastoul <kaycbas@gmail.com>
2021-03-18 20:36:05 -0700

    add merge k linked lists

--- javascript/merge-k-linked-lists.js
+++ javascript/merge-k-linked-lists.js
@@ -11,42 +11,25 @@
 //  * @param {ListNode[]} lists
 //  * @return {ListNode}
 //  */
+
+// O(nlogn) time | O(n) space
 var mergeKLists = function(lists) {
-    if (lists.length === 0) return [];
-    while(lists.length > 1) {
-        const list1 = lists.pop();
-        const list2 = lists.pop();
-        const newMerged = merge(list1, list2);
-        lists.unshift(newMerged);
-    }
-    return lists[0];    
-};
-
-function merge(node1, node2) {
-    const head1 = node1;
-    const head2 = node2;
-    let headWinner = undefined;
-
-    while (node1 !== null && node2 !== null) {
-        if (node1.val < node2.val) {
-            if (headWinner === undefined) headWinner = 'node1';
-            if (node1.next === null || node1.next.val > node2.val) {
-                const temp = node1.next;
-                node1.next = node2;
-                node1 = temp;
-            } else {
-                node1 = node1.next;   
-            }
-        } else {
-            if (node2.next === null || node2.next.val > node1.val) {
-                if (headWinner === undefined) headWinner = 'node2';
-                const temp = node2.next;
-                node2.next = node1;
-                node2 = temp;
-            } else {
-                node2 = node2.next;   
-            }
+    const vals = [];
+    for (const list of lists) {
+        let ptr = list;
+        while(ptr !== null) {
+            vals.push(ptr.val);
+            ptr = ptr.next;
         }
     }
-    return headWinner = 'node1' ? head1 : head2;
-}
+    vals.sort((a, b) => a - b);
+
+    if (vals.length === 0) return null;
+    const head = new ListNode(vals[0], null);
+    let ptr = head;
+    for (let i = 1; i < vals.length; i++) {
+        ptr.next = new ListNode(vals[i], null);
+        ptr = ptr.next;
+    }
+    return head;
+};
