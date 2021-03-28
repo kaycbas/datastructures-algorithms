d3b509ce9b2adab7653e2ffa6805dacc12f1b6e3
Kevin Bastoul <kaycbas@gmail.com>
2021-03-27 22:19:46 -0700

    add array monkey patching

---
+++ javascript/array-monkey-patching.js
@@ -0,0 +1,26 @@
+// Write an `Array.prototype.myEach(callback)` method that invokes a callback
+// for every element in an array and returns undefined. Do NOT use the built-in
+// `Array.prototype.forEach`.
+Array.prototype.myEach = function (cb) {
+    for (let i = 0; i < this.length; i++) {
+        const ele = this[i];
+        cb(ele);
+    }
+}
+
+// Write an `Array.prototype.myReduce(callback, acc)` method which takes a 
+// callback and an optional argument of a default accumulator. If myReduce only 
+// receives one argument, then use the first element of the array as the default 
+// accumulator. Use the `Array.prototype.myEach` method you defined above. Do 
+// NOT call in the built-in `Array.prototype.reduce` or `Array.prototype.forEach` 
+// methods.
+Array.prototype.myReduce = function(cb, acc) {
+    const arr = this.slice();
+    if (acc === undefined) acc = arr.shift();
+
+    arr.myEach((el) => {
+        acc = cb(acc, el);
+    });
+    
+    return acc;
+}
