42568dec071ea4fdf19c7418c3726d0ed8b709aa
Kevin Bastoul <kaycbas@gmail.com>
2021-03-29 17:13:01 -0700

    add my-join

--- javascript/array-monkey-patching.js
+++ javascript/array-monkey-patching.js
@@ -35,3 +35,17 @@ Array.prototype.myMap = function(cb) {
     
     return mapped;
 }
+
+// Write an `Array.prototype.myJoin(separator)` method, which joins the elements
+// of an array into a string. If an argument is provided to `myJoin`, use that
+// between each element. Otherwise, use an empty string.
+// Do NOT call the built-in `Array.prototype.join` method.
+// ex.
+// [1, 2, 3].myJoin() => '123'
+// [1, 2, 3].myJoin('$') => '1$2$3'
+Array.prototype.myJoin = function(seperator = '') {
+    return this.reduce((str, ele, idx) => {
+        return (idx === 0) ? str + ele : str + seperator + ele;
+    }, '');
+}
+
