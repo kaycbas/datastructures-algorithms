f3d111a6e04773bfc23855755345a725a9d053c8
Kevin Bastoul <kaycbas@gmail.com>
2021-03-29 17:31:19 -0700

    add my-every

--- javascript/array-monkey-patching.js
+++ javascript/array-monkey-patching.js
@@ -49,3 +49,15 @@ Array.prototype.myJoin = function(seperator = '') {
     }, '');
 }
 
+// Write an `Array.prototype.myEvery(callback)` method that returns true 
+// if the callback returns true for every element in the array, and otherwise 
+// returns false. Use the `Array.prototype.myEach` method you defined above. Do 
+// NOT call the built-in `Array.prototype.every` or `Array.prototype.forEach` 
+// methods.
+Array.prototype.myEvery = function(cb) {
+    let isEvery = true;
+    this.myEach(el => {
+        if (!cb(el)) isEvery = false;
+    });
+    return isEvery;
+}
