84eb3ce45aaa57a2d846ef453dff3ce1b29458fd
Kevin Bastoul <kaycbas@gmail.com>
2021-04-12 14:04:39 -0700

    add calculate risk

---
+++ javascript/note-passing.js
@@ -0,0 +1,26 @@
+// Google technical phone screen
+function calculateRisk(path) {
+    let totalSuccessRate = 1;
+
+    for (let i = 0; i < path.length - 1; i++) {
+        const loc1 = path[i];
+        const loc2 = path[i + 1];
+        
+        if (loc1[0] === loc2[0]) { // vertical pass
+            let minRow = Math.min(loc1[1], loc2[1]);
+            let successRate = 1 - (0.5 * (0.5 ** minRow));
+            console.log(successRate);
+            totalSuccessRate *= successRate;
+        } else { // horizontal pass
+            let successRate = 1 - (0.9 * (0.5 ** loc1[1]));
+            console.log(successRate);
+            totalSuccessRate *= successRate;
+        }
+    }
+
+    return (1 - totalSuccessRate) * 100;
+}
+// [x, y] => [col, row]
+let path = [[0, 0], [0, 1], [1, 1]];
+
+console.log(calculateRisk(path)); // 72.5% risk
