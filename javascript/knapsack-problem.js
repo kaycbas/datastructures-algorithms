275d9ef0d5272a24fb3aed6c6cafa7f4ed1c2144
Kevin Bastoul <kaycbas@gmail.com>
2021-04-13 17:44:53 -0700

    update knapsack problem

--- javascript/knapsack-problem.js
+++ javascript/knapsack-problem.js
@@ -27,3 +27,42 @@ function knapsackProblem(items, capacity) {
 
 // Do not edit the line below.
 exports.knapsackProblem = knapsackProblem;
+
+// ---
+// with indices...
+//
+// O(nc) time | O(nc) space
+function knapsackProblem(items, capacity) {
+  let maxVals = new Array(items.length + 1).fill(undefined);
+	maxVals = maxVals.map(ele => new Array(capacity + 1).fill(0));
+	
+	for (let w = 0; w <= capacity; w++) {
+		maxVals[0][w] = [0, []];
+	}
+	
+	for (let i = 1; i <= items.length; i++) {
+		const [itemVal, itemWeight] = items[i-1];
+		
+		for (let w = 0; w <= capacity; w++) {
+			if (itemWeight > w) {
+				maxVals[i][w] = maxVals[i - 1][w];
+			} else {
+				const [oldVal, oldIndices] = maxVals[i - 1][w];
+				const [tempVal, tempIndices] = maxVals[i - 1][w - itemWeight];
+				const [newVal, newIndices] = [tempVal + itemVal, tempIndices.concat([i - 1])];
+				
+				if (newVal > oldVal) {
+					maxVals[i][w] = [newVal, newIndices];
+				} else {
+					maxVals[i][w] = [oldVal, oldIndices];
+				}
+			}
+		}
+	}
+	
+	return maxVals[items.length][capacity];
+}
+
+// Do not edit the line below.
+exports.knapsackProblem = knapsackProblem;
+
